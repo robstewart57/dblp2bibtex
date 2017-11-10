f2d95e435390d717d4ed32f953169374f834af0c
Rob Stewart <robstewart57@gmail.com>
2017-11-10 10:38:01 +0000

    Supports DBLP retry-after 429 HTTP response

--- src/DBLPToBibtex.hs
+++ src/DBLPToBibtex.hs
@@ -5,6 +5,7 @@ module DBLPToBibtex (
     dblpToBibtexAuthorList
   ) where
 
+import Control.Concurrent
 import Control.Exception
 import Control.Monad
 import Data.Char
@@ -12,6 +13,7 @@ import Data.Either
 import Data.RDF hiding (triple)
 import qualified Data.Text as T
 import Network.HTTP
+import Network.HTTP.Headers
 import Network.Browser
 import System.IO
 
@@ -32,6 +34,7 @@ dblpToBibtexAuthor userId = do
   putStr ("searching for author " ++ userId ++ "\n")
   let firstInitial = toLower (head userId)
   let uri = "http://dblp.uni-trier.de/pers/tr/" ++ [firstInitial] ++ "/" ++ userId ++ ".nt"
+  -- putStrLn ("downloading " ++ uri)
   result <- parseURL NTriplesParser uri :: IO (Either ParseFailure (RDF TList))
   bibtexData <- case result of
     Left err -> do
@@ -48,27 +51,45 @@ dblpToBibtexAuthor userId = do
             map (\triple ->
                    let (UNode pubUri) = objectOf triple
                    in T.unpack pubUri ++ ".bib") triples
-      mapM downloadUri publicationUris
+      mapM (downloadUri 0) publicationUris
   return (unlines (rights bibtexData))
 
-downloadUri :: String -> IO (Either String String)
-downloadUri url = do
-  (_, rsp) <- Network.Browser.browse $ do
-    setAllowRedirects True -- handle HTTP redirects
-    setOutHandler (const (return ()))
-    setErrHandler (const (return ()))
-    request $ getRequest url
-  case rspCode rsp of
-   (3,0,3) -> do
-     let err = "cannot download: " ++ show url
-     hPutStr stderr err
-     return (Left err)
-   (4,0,4) -> do
-     let err = "cannot be found: " ++ show url
-     hPutStr stderr err
-     return (Left err)
-   (2,0,0) ->
-     return (Right (rspBody rsp))
-   (4,2,9) ->
-     return (Right "we're making too many requests")
-   code -> error (show code)
+downloadUri :: Int -> String -> IO (Either String String)
+downloadUri retryCount url =
+  -- arbitrarily chosen number
+  if retryCount > 3
+  then do
+    let err = "we are repeatedly making too many DBLPrequests, giving up."
+    hPutStr stderr err
+    return (Left err)
+  else do
+    (_, rsp) <- Network.Browser.browse $ do
+      setAllowRedirects True -- handle HTTP redirects
+      setOutHandler (const (return ()))
+      -- setErrHandler (const (return ()))
+      request $ getRequest url
+    case rspCode rsp of
+      (3,0,3) -> do
+        let err = "cannot download: " ++ show url
+        hPutStr stderr err
+        return (Left err)
+      (4,0,4) -> do
+        let err = "cannot be found: " ++ show url
+        hPutStr stderr err
+        return (Left err)
+      -- to many requests
+      (4,2,9) -> do
+        let headers = rspHeaders rsp
+            retryAfter :: Int
+            retryAfter = findRetry headers
+            findRetry [] = error "cannot find Retry-After header"
+            findRetry (Header HdrRetryAfter i : hdrs) = read i
+            findRetry (_ : hdrs) = findRetry hdrs
+            -- add another second to be sure
+        putStrLn $
+          "too many DBLP requests, retrying in " ++ (show (retryAfter + 1)) ++ " seconds."
+        threadDelay ((retryAfter + 1) * 1000000)
+        downloadUri (retryCount + 1) url
+      (2,0,0) ->
+        return (Right (rspBody rsp))
+      code -> error (show code)
