f050f8a14d43a217ebfcc9d70d7d5de4e0b06407
Rob Stewart <robstewart57@gmail.com>
2024-04-22 11:45:11 +0100

    Separates out obtaining paper URIs from downloading paper bibtex

--- src/DBLPToBibtex.hs
+++ src/DBLPToBibtex.hs
@@ -1,11 +1,13 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-module DBLPToBibtex (
-    dblpToBibtexAuthor,
-    dblpToBibtexAuthorList
-  ) where
+module DBLPToBibtex
+  ( dblpToBibtexAuthor,
+    dblpToBibtexAuthorList,
+  )
+where
 
+import BibtexProcessing
 import Control.Concurrent
 import Control.Exception
 import Control.Exception.Lifted
@@ -14,73 +16,81 @@ import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as BS
 import Data.Char
 import Data.Either
+import Data.List (nub)
+import Data.Maybe
 import Data.RDF hiding (triple)
 import qualified Data.Text as T
 import Data.Text.Encoding as E
+import Network.Browser
 import Network.HTTP
 import Network.HTTP.Conduit
 import Network.HTTP.Headers
-import Network.Browser
 import System.IO
 
-import BibtexProcessing
-
--- example userId: Stewart:Robert_J
--- "http://dblp.uni-trier.de/pers/tr/s/" ++ userId ++ "=.nt"
-
--- example authorOf object: StewartMBGW16
--- "http://dblp.org/rec/bib2/conf/ica3pp/" ++ authorOfURI ++ ".bib"
+dblpToBibtexAuthor :: String -> IO T.Text
+dblpToBibtexAuthor authorPid = dblpToBibtexAuthorList [authorPid]
 
 dblpToBibtexAuthorList :: [String] -> IO T.Text
-dblpToBibtexAuthorList authors =
-  T.concat <$> mapM dblpToBibtexAuthor authors
+dblpToBibtexAuthorList authorPids = do
+  allPaperURIs <- concat <$> mapM dblpPidToPaperURIs authorPids
+  let allPaperURIsNoDuplicates = nub allPaperURIs
+      howManyDuplicates = length allPaperURIs - length allPaperURIsNoDuplicates
+  when (howManyDuplicates > 0) $ putStrLn ("removing " <> show howManyDuplicates <> " duplicate papers for co-authored papers")
+  (T.concat . rights) <$> mapM paperUriToBib allPaperURIsNoDuplicates
 
-dblpToBibtexAuthor :: String -> IO T.Text
-dblpToBibtexAuthor userId = do
-  putStrLn ("\nSearching for author " ++ userId)
-  let firstInitial = toLower (head userId)
-  let uri = "https://dblp.uni-trier.de/pers/tr/" ++ [firstInitial] ++ "/" ++ userId ++ ".nt"
+paperUriToBib :: T.Text -> IO (Either String T.Text)
+paperUriToBib paperUri = do
+  downloadUri 0 (paperUri <> ".bib")
+
+dblpPidToPaperURIs :: String -> IO [T.Text]
+dblpPidToPaperURIs dblpPid = do
+  putStrLn ("\nSearching for author " ++ dblpPid)
+  let uri = "https://dblp.org/pid/" ++ dblpPid ++ ".nt"
   -- putStrLn ("downloading " ++ uri)
   result <- parseURL NTriplesParser uri :: IO (Either ParseFailure (RDF TList))
-  bibtexData <- case result of
+  paperUris <- case result of
     Left err -> do
-      hPutStr stderr ("cannot find author " ++ userId ++ "\n\n" ++ show err ++ "\n")
-      return [Left ""]
+      hPutStr stderr ("cannot find author " ++ dblpPid ++ "\n\n" ++ show err ++ "\n")
+      return Nothing
     Right rdfGraph -> do
       let triples =
             query
-            rdfGraph
-            Nothing
-            (Just (unode "https://dblp.uni-trier.de/rdf/schema-2017-04-18#authorOf"))
-            Nothing
+              rdfGraph
+              Nothing
+              (Just (unode "https://dblp.org/rdf/schema#authorOf"))
+              Nothing
           publicationUris =
-            map (\triple ->
-                   let (UNode pubUri) = objectOf triple
-                   in T.unpack pubUri ++ ".bib") triples
-      mapM (downloadUri 0) publicationUris
-  return (T.unlines (rights bibtexData))
+            map
+              ( \triple ->
+                  let (UNode pubUri) = objectOf triple
+                   in pubUri
+              )
+              triples
+      return (Just publicationUris)
+  return (fromMaybe [] paperUris)
 
-downloadUri :: Int -> String -> IO (Either String T.Text)
+downloadUri :: Int -> T.Text -> IO (Either String T.Text)
 downloadUri retryCount url =
   -- arbitrarily chosen number
   if retryCount > 3
-  then do
-    let err = "we are repeatedly making too many DBLPrequests, giving up."
-    hPutStr stderr err
-    return (Left err)
-  else do
-    putStr ("downloading " ++ url)
-    if (T.pack "http:") `T.isPrefixOf` (T.pack url)
-    then downloadHttp retryCount url
-    else if (T.pack "https:") `T.isPrefixOf` (T.pack url)
-    then downloadHttps retryCount url
-    else error ("unknown prefix for URL: " ++ url)
+    then do
+      let err = "we are repeatedly making too many DBLPrequests, giving up."
+      hPutStr stderr err
+      return (Left err)
+    else do
+      putStr ("downloading " ++ T.unpack url)
+      if (T.pack "http:") `T.isPrefixOf` url
+        then downloadHttp retryCount url
+        else
+          if (T.pack "https:") `T.isPrefixOf` url
+            then downloadHttps retryCount url
+            else error ("unknown prefix for URL: " ++ T.unpack url)
 
-downloadHttps :: Int -> String -> IO (Either String T.Text)
+downloadHttps :: Int -> T.Text -> IO (Either String T.Text)
 downloadHttps retryCount url = do
-  result <- Control.Exception.Lifted.try $ simpleHttp url
+  result <- Control.Exception.Lifted.try $ simpleHttp (T.unpack url)
   case result of
-    Left (ex::HttpException) ->
+    Left (ex :: HttpException) ->
       case ex of
         (HttpExceptionRequest req content) ->
           case content of
@@ -96,36 +106,36 @@ downloadHttps retryCount url = do
       let s = E.decodeUtf8 (BS.toStrict bs)
       return (Right s)
 
-downloadHttp :: Int -> String -> IO (Either String T.Text)
-downloadHttp retryCount url = do    
-    (_, rsp) <- Network.Browser.browse $ do
-      setAllowRedirects True -- handle HTTP redirects
-      setOutHandler (const (return ()))
-      -- setErrHandler (const (return ()))
-      request $ getRequest url
-    case rspCode rsp of
-      (3,0,3) -> do
-        let err = "cannot download: " ++ show url
-        hPutStr stderr err
-        return (Left err)
-      (4,0,4) -> do
-        let err = "cannot be found: " ++ show url
-        hPutStr stderr err
-        return (Left err)
-      -- to many requests
-      (4,2,9) -> do
-        let headers = rspHeaders rsp
-            retryAfter :: Int
-            retryAfter = findRetry headers
-            findRetry [] = error "cannot find Retry-After header"
-            findRetry (Header HdrRetryAfter i : hdrs) = read i
-            findRetry (_ : hdrs) = findRetry hdrs
-            -- add another second to be sure
-        putStrLn $
-          "\ntoo many DBLP requests, retrying in " ++ show (retryAfter + 1) ++ " seconds."
-        threadDelay ((retryAfter + 1) * 1000000)
-        downloadUri (retryCount + 1) url
-      (2,0,0) -> do
-        putStr " ... downloaded.\n"
-        return (Right (T.pack (rspBody rsp)))
-      code -> error (show code)
+downloadHttp :: Int -> T.Text -> IO (Either String T.Text)
+downloadHttp retryCount url = do
+  (_, rsp) <- Network.Browser.browse $ do
+    setAllowRedirects True -- handle HTTP redirects
+    setOutHandler (const (return ()))
+    -- setErrHandler (const (return ()))
+    request $ getRequest (T.unpack url)
+  case rspCode rsp of
+    (3, 0, 3) -> do
+      let err = "cannot download: " ++ show url
+      hPutStr stderr err
+      return (Left err)
+    (4, 0, 4) -> do
+      let err = "cannot be found: " ++ show url
+      hPutStr stderr err
+      return (Left err)
+    -- to many requests
+    (4, 2, 9) -> do
+      let headers = rspHeaders rsp
+          retryAfter :: Int
+          retryAfter = findRetry headers
+          findRetry [] = error "cannot find Retry-After header"
+          findRetry (Header HdrRetryAfter i : hdrs) = read i
+          findRetry (_ : hdrs) = findRetry hdrs
+      -- add another second to be sure
+      putStrLn $
+        "\ntoo many DBLP requests, retrying in " ++ show (retryAfter + 1) ++ " seconds."
+      threadDelay ((retryAfter + 1) * 1000000)
+      downloadUri (retryCount + 1) url
+    (2, 0, 0) -> do
+      putStr " ... downloaded.\n"
+      return (Right (T.pack (rspBody rsp)))
+    code -> error (show code)
